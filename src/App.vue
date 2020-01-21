5ab71fab93845d31dac7eb694c2b05a30129a6d2
Anton <scoutrul@mail.ru>
2020-01-21 15:18:38 +0300

    bottle

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Бутылочка</h1>
+    <Bottle :fillIn="+fillIn" :fillOut="+fillOut"/>
+    <input v-model="fillIn" placeholder="Наполннить" type="number">
+    <input v-model="fillOut" placeholder="Слить" type="number">
+  </div>
+</template>
+
+<script>
+import Bottle from './components/Bottle.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Bottle
+  },
+  data: () => ({
+    fillIn: 0,
+    fillOut: 0
+  })
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Bottle.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h2>Объем</h2>
+    <div>{{ counted }}</div>
+    <button @click="count">рассчитать </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bottle",
+  props: {
+    fillIn: {
+      type: Number,
+      require: false,
+      default: () => 0
+    },
+    fillOut: {
+      type: Number,
+      require: false,
+      default: () => 0
+    },
+    vol: {
+      type: Number,
+      require: false,
+      default: () => 1000
+    }
+  },
+  computed: {
+    counted(){
+      return +this.fillIn + -this.fillOut
+    }
+  },
+  methods: {
+    count(){
+      if(this.counted < 0) {
+        alert('переслив')
+      }
+      else if(this.counted > this.vol) {
+        alert('перелив')
+      }
+      else {
+        alert(`в бутылке ${this.counted} ml`)
+      }
+    }   
+  }
+};
+</script>
+
+<style scoped></style>
